cb794744564dc8fef39883d628cbd57319529199
Vinicius Biondo <vkatatab@gmail.com>
2017-10-04 23:08:00 -0300

    Adicionando overhang

---
+++ classes/overhang1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Essa classe MonteCarlo é o algoritmo responsavel por gerar as variações randomicas dos parâmetros
+
+import random
+import math
+
+class Overhang1(object):
+
+    def __init__(self, parameters):
+        self.newValue = 0;
+        orientation = int(parameters)
+        precision = 0.01
+        minNumber = 0.01
+        maxNumber = 0.01
+        if (orientation == 0):
+            maxNumber = 0.36
+        elif (orientation == 45):
+            maxNumber = 1.19
+        elif (orientation == 90):
+            maxNumber = 2.75
+        elif (orientation == 135):
+            maxNumber = 2.75
+        elif (orientation == 180):
+            maxNumber = 1.73
+        elif (orientation == 225):
+            maxNumber = 2.75
+        elif (orientation == 270):
+            maxNumber = 2.75
+        elif (orientation == 315):
+            maxNumber = 1.19
+
+        rand = random.randint(0,math.floor((maxNumber - minNumber)/precision))
+        self.newValue = round ((rand * precision) + minNumber, 2)
+
+    def getNewValue(self):
+        return str(self.newValue)
