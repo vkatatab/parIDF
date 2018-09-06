57a20bf7107de4797bdf4d1c1220a47a59db92b3
Vinicius Biondo <vkatatab@gmail.com>
2018-09-06 13:59:29 -0300

    discretizacao dos valores de input

--- classes/predefined.py
+++ classes/predefined.py
@@ -10,8 +10,15 @@ class Predefined(object):
     def __init__(self, parameters):
         self.newValue = 0;
         values = parameters['values']
+        if 'text' in parameters:
+            self.text = parameters['text']
+        else:
+            self.text = ''
         rand = random.randint(0,len(values)-1)
         self.newValue = values[rand]
 
     def getNewValue(self):
-        return str(self.newValue)
+        value = str(self.newValue)
+        if not self.text:
+            return value
+        return self.text + value
